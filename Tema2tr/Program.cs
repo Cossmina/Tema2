d8cf2667046460c91e487ab27404c03fd5c31830
TEZI COSMINA <cosmina.tezi1@gmail.com>
2018-12-13 12:33:46 +0200

    Rezolvare ex1 tema 2

--- Tema2tr/Program.cs
+++ Tema2tr/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace Tema2tr
 {
+    public enum Persoana
+    {
+        male,
+        female
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            char? gen;
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("Introduceti data la care v-ati nascut(mm/dd/yyyy)");
+            DateTime dataNasterii = DateTime.Parse(Console.ReadLine());
+            //Calculul varstei
+            TimeSpan varstaInZile = now - dataNasterii;
+            int varsta = varstaInZile.Days / 365;
+            Console.WriteLine("Varsta dumneavoastra este:" + " " + varsta);
+            
+            Console.WriteLine("Introduceti genul (male - m, female - f)");
+            gen = char.Parse(Console.ReadLine());
+
+            if (gen == 'f'& varsta<63)
+            {
+                Console.WriteLine(Persoana.female + " " + "at" + " " + varsta);
+            }
+            else if (gen == 'f' & varsta>=63)
+            {
+                Console.WriteLine(Persoana.female +" " + "retired");
+            }
+
+            else if (gen == 'm'& varsta<65)
+            {
+                Console.WriteLine(Persoana.male +" "+ "at" +" "+ varsta);
+            }
+            else if(gen == 'm' & varsta >= 65)
+            {
+                Console.WriteLine(Persoana.male + " " + "retired");
+            }
+
+            else
+                Console.WriteLine("Nu ati introdus un gen valid");
+
+            Console.ReadKey();
+
         }
     }
 }
